7fc88a63ec64e0c0f64de38ca378860e7ff225f6
vishwaschhimpa <vishwaschhimpa27@gmail.com>
2022-02-27 02:20:18 +0530

    nav bar 2

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,42 @@
 <template>
-<div>
-  <ul></ul>
+<div id="navbar">
+  <v-toolbar app fixed :class="{'elevation-0': this.navbar_transparent}">
+      <v-toolbar-title>
+        App Name
+      </v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-toolbar-items>
+        <v-btn flat to="/home">Home</v-btn>
+        <v-btn flat @click="scrollTop">Login</v-btn>
+      </v-toolbar-items>
+  </v-toolbar>
 </div>
-  <div id="bg">
-  <p>"HelloWorld"</p>
-    
-  </div>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your BAE VERSE"/>
+ 
 </template>
 
 <script>
-
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld  
-  }
+  name: "Navbar",
+  data() {
+    return {
+      is_transparent: true
+    }
+  },
+  methods: {
+    onScroll(e) {
+      this.is_transparent = !(e.target.scrollTop > 0);
+    }
+  },
 }
+
+
 </script>
+
 
 <style>
 
 
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-  background-color: rgb(108, 175, 175);
-
-}
 </style>
